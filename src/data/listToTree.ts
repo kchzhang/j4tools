f025c617826e05ebd8890f3b5413e26a86bf3fe6
knox.zhang <knox.zhang@wokaidai.com>
2021-09-15 11:35:28 +0800

    fix listToTree

--- src/data/listToTree.ts
+++ src/data/listToTree.ts
@@ -1,9 +1,8 @@
 import { listTree, PowerPartial, Options } from '../.internal/listTree';
 
- function listToTree<T>(node: Array<PowerPartial<T>>, option: Options): Array<PowerPartial<T>> {
-   return listTree(node, option);
- }
- 
- export default listToTree;
- 
- 
+function listToTree<T>(node: Array<PowerPartial<T>>, option: Options): Array<PowerPartial<T>> {
+  return listTree(node, option);
+}
+
+export default listToTree;
+
